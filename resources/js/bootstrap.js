86ea7e0b0fa4afa8e3978ea55d8265cb96bbc415
Kenkenetoy <kennethjoseaparece@gmail.com>
2025-02-07 02:15:30 +0800

    Update bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -10,4 +10,19 @@ axios.defaults.baseURL = 'http://127.0.0.1:8000';
  * allow your team to quickly build robust real-time web applications.
  */
 
-import './echo';
+// import './echo';
+
+import Echo from 'laravel-echo';
+
+import Pusher from 'pusher-js';
+window.Pusher = Pusher;
+
+window.Echo = new Echo({
+    broadcaster: 'reverb',
+    key: import.meta.env.VITE_REVERB_APP_KEY,
+    wsHost: import.meta.env.VITE_REVERB_HOST,
+    wsPort: import.meta.env.VITE_REVERB_PORT,
+    wssPort: import.meta.env.VITE_REVERB_PORT,
+    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    enabledTransports: ['ws', 'wss'],
+});
